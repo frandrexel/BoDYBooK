363c1176db39734f78cbf6a2e5381aa6e17c0490
frandrexel <fg349@drexel.edu>
2024-10-07 20:56:53 -0400

    Delete untitled directory

--- untitled/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_untitled_FILE C:/Users/skamb/Documents/GitHub/BoDYBooK/untitled/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/untitled.exe)
-set(__QT_DEPLOY_TARGET_untitled_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_untitled_RUNTIME_DLLS C:/Qt/6.7.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
