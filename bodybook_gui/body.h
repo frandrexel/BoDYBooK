ccc0accd5f797248e7c8d33097251a830796e7a5
frandrexel <fg349@drexel.edu>
2024-10-09 00:54:38 -0400

    Update bodybook_gui

---
+++ bodybook_gui/body.h
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Cephalic{
+  public:
+}
+
+class Cervical{
+  public:
+}
+
+class Thoraicc{
+  public:
+}
+
+class Abdominal{
+  public:
+}
+
+class Pelvic {
+  public:
+}
+
+class Pubic {
+  public:
+}
+
+class Lower limb {
+  public:
+}
+
+ 
+class Upper limb {
+  public:
